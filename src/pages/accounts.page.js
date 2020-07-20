528aad92141b9f6fc4e31d44c57059a1329581f0
alex-s85 <worldmaster5558@hotmail.com>
2020-07-20 18:45:52 +0200

    Logic for saving dummy data to DB

--- src/pages/accounts.page.js
+++ src/pages/accounts.page.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import ProfileCard from '../components/ProfileCard';
 import AccountsDataService from '../services/AccountsService';
 
+const { users } = require('../dummy_data.json');
+
 const AccountsPage = () => {
   const [accounts, setAccounts] = useState([]);
 
@@ -20,14 +22,39 @@ const AccountsPage = () => {
       });
   };
 
+  const saveDummyDataToDB = () => {
+    users.map(item => {
+      return AccountsDataService.create(item)
+      .then(response => {
+        retrieveAllAccounts();
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    });
+  }
+
   return (
     <div class="container" >
       <div class="row">
         <div class="col-sm-9 row justify-content-around">
-          {accounts.map(item => <ProfileCard user={item}/>)}          
+          {accounts.length > 0 ?
+            accounts.map(item => <ProfileCard user={item}/>)
+            :
+            <p>
+              {`No accounts`}
+            </p>
+          }          
         </div>
-        <div class="col-sm-3 border-left align-middle">
-          {`Click the card to see the details.`}
+        <div class="col-sm-3 border-left">
+          <button 
+            class="btn btn-primary"
+            onClick={() => saveDummyDataToDB()}
+            disabled={accounts.length > 0}>
+            {'Insert dummy data'}
+          </button>
+          <p>{`Click the card to see the details.`}</p>
         </div>
       </div>
     </div>
