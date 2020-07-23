bfc827c5763e422686aae81bce46bad8ce948f86
alex-s85 <worldmaster5558@hotmail.com>
2020-07-23 04:20:23 +0200

    responsive card

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProfileCard = ({user}) => {
   return (    
-    <div className="card col-4">
+    <div className="card col-lg-4">
       <img src={`${user.picture}`} className="card-img-top rounded-circle" alt={'Avatar'}/>
       <div className="card-body">
         <h5 className="card-title">{`${user.nameFirst} ${user.nameLast}`}</h5>
